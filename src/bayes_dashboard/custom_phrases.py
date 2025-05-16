9f61ca2acc745c9ad1330ef6f4449effc9c61c32
Sam Hill <sam.j.hill@protonmail.com>
2025-05-16 18:15:22 +0100

    Adds in a custom phrase to allow for customisation of dashboard

---
+++ src/bayes_dashboard/custom_phrases.py
@@ -0,0 +1,23 @@
+phrases = {
+    'Farmer Steve': {
+        'prior': 'of the population are librarians',
+        'likelihood': 'of librarians are shy',
+        'false_pos': 'of farmers are shy',
+        'marginal': 'What percentage of the total population are shy?',
+        'posterior': 'What percentage of shy people are librarians?'
+    },
+    'Test Efficacy': {
+        'prior': ' of the population have the disease',
+        'likelihood': 'The test has a sensitivity of',
+        'false_pos': ' The test has a false positive rate of',
+        'marginal': 'What percentage of the total population tested positive?',
+        'posterior': 'What is the change of having the disease given a positive test?'  # noqa: E501
+    },
+    'Generic Bayes': {
+        'prior': 'The prior is',
+        'likelihood': 'The likelihood is',
+        'false_pos': 'The False Positive Rate is',
+        'Marginal': 'We are likely to see evidence from the total population at a rate of',  # noqa: E501
+        'posterior': 'What is probability of hypothesis being true given the evidence?'  # noqa: E501
+    }
+}
