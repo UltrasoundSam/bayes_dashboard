80d89c48df4d56ae3e717421c6308db40e017891
Sam Hill <sam.j.hill@protonmail.com>
2025-05-17 11:55:28 +0100

    Adds in Posterior calculation explanation and customisation

--- src/bayes_dashboard/posterior_info.py
+++ src/bayes_dashboard/posterior_info.py
@@ -2,6 +2,7 @@ from dash import html, Dash
 from dash import Input, Output
 
 from . import ids
+from .custom_phrases import PHRASES
 
 
 def render(app: Dash) -> html.Div:
@@ -11,11 +12,13 @@ def render(app: Dash) -> html.Div:
         Output(ids.POSTERIOR_INFO, 'children'),
         [Input(ids.PRIOR_SLIDER, "value"),
          Input(ids.LIKE_SLIDER, "value"),
-         Input(ids.FALSE_SLIDER, "value")]
+         Input(ids.FALSE_SLIDER, "value"),
+         Input(ids.SCENARIO_DROPDOWN, 'value')]
     )
     def bayes_rule(prior: int,
                    likelihood: int,
-                   false_positive: int) -> html.Div:
+                   false_positive: int,
+                   scenario: str) -> html.Div:
         '''Applies Bayes rule to update out prediction'''
         # Calculate True positives
         true_positives = (prior/100)*(likelihood/100)
@@ -24,11 +27,32 @@ def render(app: Dash) -> html.Div:
         # Calculates Marginal
         marginal = true_positives + false_positives
 
+        # Applying Bayes Rule
         posterior = true_positives / (marginal)
 
-        return html.Div([
-            html.P(f'We should update our beliefs from {prior:.1f}% to {posterior:.1%}')
-        ])
+        # Getting custom message from dict
+        cust_msg = PHRASES[scenario]['posterior']
 
+        return html.Div([
+            html.H4('Posterior, P(H|E)'),
+            html.P(cust_msg),
+            html.H3(f'{posterior:.1%}'),
+            html.P("Calculated using Bayes' Theorem as:"),
+            html.H5(f'{true_positives:.1%}',
+                    style={'background-color': '#f2767b',
+                           'width': '15%',
+                           'margin-left': '42.5%'}),
+            html.Hr(style={'width': '32%',
+                           'margin-left': '34%'}),
+            html.H5(
+                [
+                    html.Span(f'{true_positives:.1%}',
+                              style={'background-color': '#f2767b'}),
+                    html.Span(' + '),
+                    html.Span(f'{false_positives: .1%}',
+                              style={'background-color': '#7276fb'})
+                ]
+            )
+        ], style={'textAlign': 'center'})
 
     return html.Div(id=ids.POSTERIOR_INFO)
