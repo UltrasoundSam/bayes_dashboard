ec05b2a106cb1815ca92caabd3f3e74f601d552c
Sam Hill <sam.j.hill@protonmail.com>
2025-05-17 12:06:09 +0100

    Adds in gunicorn as dependency

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from dash import Dash, html
+from dash import Dash
 from dash_bootstrap_components.themes import BOOTSTRAP
 
 from bayes_dashboard import create_layout
 
-def main():
-    # Create app
-    app = Dash(external_stylesheets=[BOOTSTRAP])
-    app.title = "Bayes' Rule Dashboard"
-    app.layout = create_layout(app)
-    app.run(debug=True)
+
+# Create app
+app = Dash(external_stylesheets=[BOOTSTRAP])
+app.title = "Bayes' Rule Dashboard"
+app.layout = create_layout(app)
+server = app.server
 
 
 if __name__ == '__main__':
-    main()
+    app.run(debug=True)
